e85547450d0708677df57c2039bf41b2881a930b
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-24 19:26:12 +0900

    thisに変換することでイベント発火元となった要素を取得させ見やすいコードにした

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ window.addEventListener('load', function(){
   const pullDownParents = document.getElementById("pull-down")
   
   pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+    this.setAttribute("style", "background-color:#FFBEDA;")
   })
   pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color;#FFBEDA;")
+    this.removeAttribute("style", "background-color;#FFBEDA;")
   })
   pullDownButton.addEventListener('click', function(){
     pullDownParents.setAttribute("style", "display:block;")
